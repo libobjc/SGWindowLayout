95018bc5c04d5f3f65afe692849b35c119bafd52
Single <musicman_leehom@126.com>
2016-11-21 10:39:09 +0800

    Initial Commit

---
+++ WindowLayout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WindowLayout
+//
+//  Created by Single on 2016/11/21.
+//  Copyright © 2016年 single. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WindowLayout/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WindowLayout
+//
+//  Created by Single on 2016/11/21.
+//  Copyright © 2016年 single. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
